6365bc5dc2073fcb28214a81e5d2a315335faae8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2011-10-29 22:42:56 +0300

    Improved doctests.

--- src/wheezy/html/markup.py
+++ src/wheezy/html/markup.py
@@ -9,21 +9,47 @@ from wheezy.html.comp import iteritems
 class Tag(object):
     """
     """
-    __slots__ = ['name', 'inner', 'attr']
+    __slots__ = ['name', 'inner', 'attrs']
 
-    def __init__(self, name, inner=None, attr=None, **kwargs):
+    def __init__(self, name, inner=None, attrs=None):
         self.name = name
-        self(inner, attr, **kwargs)
+        self.inner = inner
+        self.attrs = attrs or {}
 
-    def __call__(self, inner=None, attr=None, **kwargs):
-        self.inner = repr(inner)
-        if attr:
-            if kwargs:
-                attr.update(kwargs)
-            self.attr = attr
-        else:
-            self.attr = kwargs
+    def __call__(self, inner=None, attrs=None, **kwargs):
+        """
+            ``attrs`` overwrite ``kwargs``.
+
+            >>> t = Tag('x')
+            >>> t(attrs={'a': '1', 'b': '2'}, b='3', d='4')
+            <x a="1" b="2" d="4" />
+        """
+        self.inner = inner
+        self.attrs = dict(self.attrs, **kwargs)
+        if attrs:
+            self.attrs.update(attrs)
         return self
+
+    def append_attr(self, name, value):
+        """
+            If there is no attribute with ``name`` than it added.
+
+            >>> t = Tag('x')
+            >>> t.append_attr('class', 'a')
+            >>> t
+            <x class="a" />
+
+            Existing attribute is extended with space and value.
+
+            >>> t.append_attr('class', 'b')
+            >>> t
+            <x class="b a" />
+        """
+        attrs = self.attrs
+        if name in attrs:
+            attrs[name] = value + ' ' + attrs[name]
+        else:
+            attrs[name] = value
 
     def __repr__(self):
         """
@@ -32,21 +58,25 @@ class Tag(object):
             >>> print(Tag('span')("abc", class_='error'))
             <span class="error">abc</span>
 
+            Alternatively you can use ``attr``:
+
+            >>> print(Tag('span')("abc", attrs={'class': 'error'}))
+            <span class="error">abc</span>
+
             Self closing tag
 
-            >>> t = Tag('input')
             >>> print(Tag('input')(id='name', value='abc'))
             <input id="name" value="abc" />
         """
         parts = []
         append = parts.append
         append('<' + self.name)
-        if self.attr:
-            for name, value in iteritems(self.attr):
+        if self.attrs:
+            for name, value in iteritems(self.attrs):
                 append(' ' + name.rstrip('_') +
-                        '="' + value + '"')
-        if self.inner:
-            append('>' + self.inner + '</' + self.name + '>')
+                        '="' + str(value) + '"')
+        if self.inner is not None:
+            append('>' + str(self.inner) + '</' + self.name + '>')
         else:
             append(' />')
         return ''.join(parts)
@@ -66,3 +96,6 @@ class Fragment(object):
 
     def __repr__(self):
         return ''.join(map(repr, self.tags))
+
+    def __getattr__(self, name):
+        return getattr(self.tags[0], name)
