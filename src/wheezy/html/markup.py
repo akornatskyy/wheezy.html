0c6a5c631707d9d0c30b23d1a858880ea59a2fe9
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-02-18 19:11:29 +0200

    Eliminate Tag.__call__ function

--- src/wheezy/html/markup.py
+++ src/wheezy/html/markup.py
@@ -3,7 +3,7 @@
     http://www.w3schools.com/tags/
 """
 
-from wheezy.html.comp import iteritems
+from wheezy.html.comp import PY3
 from wheezy.html.comp import str_type
 
 
@@ -15,84 +15,65 @@ class Tag(object):
     def __init__(self, name, inner=None, attrs=None):
         self.name = name
         self.inner = inner
-        self.attrs = attrs or {}
-
-    def __call__(self, inner=None, attrs=None, **kwargs):
-        """
-            ``attrs`` overwrite ``kwargs``.
-
-            >>> t = Tag('x')
-            >>> t(attrs={'a': '1', 'b': '2'}, b='3', d='4')
-            <x a="1" b="2" d="4" />
-        """
-        self.inner = inner
-        self.attrs = dict(self.attrs, **kwargs)
-        if attrs:
-            self.attrs.update(attrs)
-        return self
-
-    def __unicode__(self):  # pragma: nocover, python 3
-        """
-            >>> assert str_type(Tag('x')) == str_type('<x />')
-        """
-        return self.render(str_type)
-
-    def __str__(self):
-        """
-            >>> assert str(Tag('x')) == '<x />'
-        """
-        return self.render(str)
+        self.attrs = attrs
 
     def __repr__(self):
-        return self.render(str)
+        return str(self.render())
 
-    def render(self, converter):
+    def render(self):
         """
             Normal tag with attributes
 
-            >>> Tag('span')("abc", class_='error')
-            <span class="error">abc</span>
-
-            Alternatively you can use ``attr``:
-
-            >>> Tag('span')("abc", attrs={'class': 'error'})
+            >>> Tag('span', 'abc', {'class': 'error'})
             <span class="error">abc</span>
 
             Self closing tag
 
-            >>> Tag('input')(id='name', value='abc')
+            >>> Tag('input', None, {'id': 'name', 'value': 'abc'})
             <input id="name" value="abc" />
         """
         name = self.name
         attrs = self.attrs
-        if attrs:
+        if attrs is None:
+            t = '<' + name
+        else:
             t = '<' + name + ' ' + ' '.join(
                     [k.rstrip('_') + '="' + attrs[k] + '"'
                         for k in attrs])
-        else:
-            t = '<' + name
         inner = self.inner
-        if inner is not None:
-            return t + '>' + converter(inner) + '</' + name + '>'
-        else:
+        if inner is None:
             return t + ' />'
-        # 0.442
-        #parts = []
-        #append = parts.append
-        #append('<' + self.name)
-        #attrs = self.attrs
-        #if attrs:
-        #    parts.extend([' ' + name.rstrip('_') + '="' + attrs[name] + '"'
-        #        for name in attrs])
-        #    #for name, value in iteritems(self.attrs):
-        #    #    append(' ' + name.rstrip('_') +
-        #    #            '="' + value + '"')
-        #if self.inner is not None:
-        #    append('>' + converter(self.inner) +
-        #            '</' + self.name + '>')
-        #else:
-        #    append(' />')
-        #return ''.join(parts)
+        else:
+            return t + '>' + str_type(inner) + '</' + name + '>'
+
+    if PY3:
+        __str__ = render
+    else:
+        __unicode__ = render
+
+
+class SelfClosingTag(object):
+    """ Represents object version of HTML self closing tag.
+    """
+    __slots__ = ['name', 'attrs']
+
+    def __init__(self, name, attrs):
+        self.name = name
+        self.attrs = attrs
+
+    def __repr__(self):
+        return str(self.render())
+
+    def render(self):
+        attrs = self.attrs
+        return self.name + ' '.join(
+                [k.rstrip('_') + '="' + attrs[k] + '"'
+                    for k in attrs]) + ' />'
+
+    if PY3:
+        __str__ = render
+    else:
+        __unicode__ = render
 
 
 class Fragment(object):
